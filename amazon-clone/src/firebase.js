95336261ec0def3dcf0c9ec044392cfd76359487
deepanshu1422 <deepanshu1422@gmail.com>
2021-10-25 20:24:36 +0530

    till navbar

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5fNpeM46pVYJTL0MaL6Zl9LR5oYU_F9k",
+    authDomain: "challenge-c22d5.firebaseapp.com",
+    projectId: "challenge-c22d5",
+    storageBucket: "challenge-c22d5.appspot.com",
+    messagingSenderId: "1012681225612",
+    appId: "1:1012681225612:web:3e404bb90e35bf5c0252a9",
+    measurementId: "G-5QYCMCWHJ7"
+  };
